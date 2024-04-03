faf26ef0db8f5d4f0580d0387f90d624d322b182
shruti29s <shruti29s06@gmail.com>
2024-04-03 19:16:23 +0530

    Add inline style in Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,11 +4,13 @@ class Navbar extends React.Component{
     render(){
         return (
             <>
-            <div>
-                <div>Title</div>
-                <div>
-                    <img alt="Cart Icon"></img>
-                    <span>&nbsp;0</span>
+            <div style={styles.nav}>
+                <div style={styles.title}>
+                    Movie-App
+                </div>
+                <div style={styles.cartContainer}>
+                    <img alt="Cart Icon" style={styles.cartIcon} src="https://cdn-icons-png.flaticon.com/128/891/891462.png"></img>
+                    <span style={styles.cartCount}>3</span>
                 </div>
             </div>
             </>
@@ -17,3 +19,43 @@ class Navbar extends React.Component{
 }
 
 export default Navbar;
+
+const styles = {
+    nav: {
+        height: 70, 
+        background: "#4267b2", 
+        display: "flex", 
+        justifyContent: "space-between",
+        alignItems: "center",
+        position: "relative"
+    },
+
+    title: {
+        fontSize: 30,
+        color: "#fff",
+        fontWeight: 600,
+        fontFamily: '"Montserrat", sans-serif',
+        textTransform: "uppercase",
+        marginLeft: 20
+    },
+
+    cartContainer: {
+        position: "relative",
+        cursor: "pointer"
+    },
+
+    cartIcon: {
+        height: 48,
+        marginRight: 20
+    },
+
+    cartCount: {
+        background: "orange",
+        borderRadius: "50%",
+        padding: "4px 8px",
+        position: "absolute",
+        right: 10,
+        top: -5,
+        fontSize: 12
+    }
+}
