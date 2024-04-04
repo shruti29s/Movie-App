68dd61dcaca7d2eaaece295d828c518ffc267999
shruti29s <shruti29s06@gmail.com>
2024-04-04 14:16:38 +0530

    Added styled components and dynamic styles in Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,18 +1,59 @@
 import React from "react";
+import styled from "styled-components";
+
+const Nav = styled.div`
+height: 70px;
+background: linear-gradient(170deg, #1bc059, #0d47a1);
+display: flex;
+justify-content: space-between;
+align-items: center;
+position: relative;
+`;
+
+const Title = styled.div`
+font-size: 30px;
+color: #fff;
+font-weight: 600;
+font-family: 'Times NEW Roman', Times, serif;
+text-transform: uppercase;
+margin-left: 20px;
+&:hover{color: #0f0}
+`;
+
+const CartImg = styled.img`
+height: 48px;
+margin-right: 20px;
+`;
+
+const CartIconContainer = styled.div`
+position: relative;
+cursor: pointer;
+`;
+
+const CartCount = styled.div`
+background-color: ${(props) => props.color};
+border-radius: 50%;
+padding: 4px 8px;
+position: absolute;
+right: 10px;
+top: -5px;
+font-size: 12px;
+visibility: ${(props) => props.show?"visible":"hidden"};
+`;
 
 class Navbar extends React.Component{
     render(){
         return (
             <>
-            <div style={styles.nav}>
-                <div style={styles.title}>
+            <Nav>
+                <Title>
                     Movie-App
-                </div>
-                <div style={styles.cartContainer}>
-                    <img alt="Cart Icon" style={styles.cartIcon} src="https://cdn-icons-png.flaticon.com/128/891/891462.png"></img>
-                    <span style={styles.cartCount}>3</span>
-                </div>
-            </div>
+                </Title>
+                <CartIconContainer>
+                    <CartImg alt="Cart Icon" src="https://cdn-icons-png.flaticon.com/128/891/891462.png"></CartImg>
+                    <CartCount color="yellow" show={false}>3</CartCount>
+                </CartIconContainer>
+            </Nav>
             </>
         )
     }
